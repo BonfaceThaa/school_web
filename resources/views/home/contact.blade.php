4ad8b6d57cdcf207e96c5a6af545ebf74180de54
BonfaceThaa <thaabonface@gmail.com>
2020-04-22 22:49:17 +0300

    style(common): add about us and contact us content

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -1,22 +1,5 @@
 @extends('layouts.app')
 @section('content')
-       <!--================Home Banner Area =================-->
-       <section class="banner_area">
-            <div class="banner_inner d-flex align-items-center">
-            	<div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
-				<div class="container">
-					<div class="banner_content text-center">
-						<h2>Contact Us</h2>
-						<div class="page_link">
-							<a href="index.html">Home</a>
-							<a href="contact.html">Contact</a>
-						</div>
-					</div>
-				</div>
-            </div>
-        </section>
-        <!--================End Home Banner Area =================-->
-        
         <!--================Contact Area =================-->
         <section class="contact_area p_120">
             <div class="container">
@@ -25,18 +8,18 @@
                         <div class="contact_info">
                             <div class="info_item">
                                 <i class="lnr lnr-home"></i>
-                                <h6>California, United States</h6>
-                                <p>Santa monica bullevard</p>
+                                <h6>Postal address</h6>
+                                <p>P.O BOX 1806, Thika</p>
                             </div>
                             <div class="info_item">
                                 <i class="lnr lnr-phone-handset"></i>
-                                <h6><a href="#">00 (440) 9865 562</a></h6>
-                                <p>Mon to Fri 9am to 6 pm</p>
+                                <h6><a href="#">Contacts</a></h6>
+                                <p>0721 242 147 <br> 0721 156 034</p>
                             </div>
                             <div class="info_item">
                                 <i class="lnr lnr-envelope"></i>
-                                <h6><a href="#">support@colorlib.com</a></h6>
-                                <p>Send us your query anytime!</p>
+                                <h6><a href="#">Email</a></h6>
+                                <p>support@nyarischool.sc.ke</p>
                             </div>
                         </div>
                     </div>
